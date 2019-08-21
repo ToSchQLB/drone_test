448aa1342bd99a987a29cc799c59a9cd24761684
Toni Schreiber <toni.schreiber@jki.bund.de>
2019-08-21 10:24:04 +0200

    update dependencies

---
+++ migrations/m190821_074659_create_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m190821_074659_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(),
+            'passwort' => $this->string(),
+            'email' => $this->string(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
